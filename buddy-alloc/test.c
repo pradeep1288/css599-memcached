4344e04c7507232838f05d65a1b528748f6aa1b8
Varun S Lingaraju <warunsl@gmail.com>
2013-04-27 15:11:26 -0700

    Check to see if the size is accessible from the main function

--- buddy-alloc/test.c
+++ buddy-alloc/test.c
@@ -3,12 +3,13 @@
 
 int main(int argc, char const *argv[]) {
 
-    item *pointer_one;
+    void *pointer_one;
     buddy_init();
 
-    pointer_one = buddy_alloc(8);
+    pointer_one = buddy_alloc(13);
     //print_the_memory_layout();
-    buddy_free(pointer_one);
+    printf("Allocated block : %p of size : %ld\n", pointer_one, ((item*)pointer_one)->size);
+    buddy_free(&pointer_one);
     printf("\n");
 
     return 0;
