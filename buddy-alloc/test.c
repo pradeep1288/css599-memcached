70482a87946975e9ee5959e9fcd0fbcfbabd6465
Varun S Lingaraju <warunsl@gmail.com>
2013-04-27 19:51:36 -0700

    More pointer allocations to check if the free list is updated correctly

--- buddy-alloc/test.c
+++ buddy-alloc/test.c
@@ -3,13 +3,16 @@
 
 int main(int argc, char const *argv[]) {
 
-    void *pointer_one;
+    void *pointer_one, *pointer_two, *pointer_three;
     buddy_init();
 
     pointer_one = buddy_alloc(33);
-    print_the_memory_layout();
     printf("Allocated block : %p of size : %ld\n", pointer_one, ((item*)pointer_one)->size);
-    buddy_free(&pointer_one);
+    pointer_two = buddy_alloc(31);
+    printf("Allocated block : %p of size : %ld\n", pointer_two, ((item*)pointer_two)->size);
+    pointer_three = buddy_alloc(15);
+    printf("Allocated block : %p of size : %ld\n", pointer_three, ((item*)pointer_three)->size);
+    //buddy_free(&pointer_one);
     print_the_memory_layout();
 
     return 0;
