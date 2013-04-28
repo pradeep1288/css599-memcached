71a9c0ecfc296b3fc71166ebebab302b365a37a3
Varun S Lingaraju <warunsl@gmail.com>
2013-04-27 20:37:49 -0700

    Few more allocations to test the entire program's correctness

--- buddy-alloc/test.c
+++ buddy-alloc/test.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char const *argv[]) {
 
-    void *pointer_one, *pointer_two, *pointer_three;
+    void *pointer_one, *pointer_two, *pointer_three, *pointer_four;
     buddy_init();
 
     pointer_one = buddy_alloc(33);
@@ -12,6 +12,8 @@ int main(int argc, char const *argv[]) {
     printf("Allocated block : %p of size : %ld\n", pointer_two, ((item*)pointer_two)->size);
     pointer_three = buddy_alloc(15);
     printf("Allocated block : %p of size : %ld\n", pointer_three, ((item*)pointer_three)->size);
+    pointer_four = buddy_alloc(19);
+    printf("Allocated block : %p of size : %ld\n", pointer_four, ((item*)pointer_four)->size);
     //buddy_free(&pointer_one);
     print_the_memory_layout();
 
