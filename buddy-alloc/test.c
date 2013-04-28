d4264b982caac5071336df49a4fcb22e33fdb2cd
Varun S Lingaraju <warunsl@gmail.com>
2013-04-27 21:44:08 -0700

    more test allocations

--- buddy-alloc/test.c
+++ buddy-alloc/test.c
@@ -4,6 +4,7 @@
 int main(int argc, char const *argv[]) {
 
     void *pointer_one, *pointer_two, *pointer_three, *pointer_four;
+    void *pointer5, *pointer6, *pointer7, *pointer8, *pointer9, *pointer10;
     buddy_init();
 
     pointer_one = buddy_alloc(33);
@@ -14,7 +15,20 @@ int main(int argc, char const *argv[]) {
     printf("Allocated block : %p of size : %ld\n", pointer_three, ((item*)pointer_three)->size);
     pointer_four = buddy_alloc(19);
     printf("Allocated block : %p of size : %ld\n", pointer_four, ((item*)pointer_four)->size);
+    pointer5 = buddy_alloc(128);
+    printf("Allocated block : %p of size : %ld\n", pointer5, ((item*)pointer5)->size);
+    pointer6 = buddy_alloc(256);
+    printf("Allocated block : %p of size : %ld\n", pointer6, ((item*)pointer6)->size);
+    pointer7 = buddy_alloc(512);
+    printf("Allocated block : %p of size : %ld\n", pointer7, ((item*)pointer7)->size);
+    pointer8 = buddy_alloc(1024);
+    printf("Allocated block : %p of size : %ld\n", pointer8, ((item*)pointer8)->size);
+    pointer9 = buddy_alloc(2048);
+    printf("Allocated block : %p of size : %ld\n", pointer9, ((item*)pointer9)->size);
+    pointer10 = buddy_alloc(4096);
+    printf("Allocated block : %p of size : %ld\n", pointer10, ((item*)pointer10)->size);
     //buddy_free(&pointer_one);
+    test_buddy();
     print_the_memory_layout();
 
     return 0;
