990268288cd4150cebb200c7d7e94c87de42fe76
Varun S Lingaraju <warunsl@gmail.com>
2013-04-28 20:22:33 -0700

    Renamed test to merge_test and has test cases flow for buddy merge

---
+++ buddy-alloc/merge_test.c
@@ -0,0 +1,16 @@
+#include "buddy.h"
+
+
+int main(int argc, char const *argv[]) {
+
+    void *pointer_one, *pointer_two, *pointer_three;
+    pointer_one = buddy_alloc(32);
+    print_the_memory_layout();
+    pointer_two = buddy_alloc(16);
+    print_the_memory_layout();
+    buddy_free(&pointer_two);
+    print_the_memory_layout();
+    pointer_three = buddy_alloc(32);
+    print_the_memory_layout();
+    return 0;
+}
